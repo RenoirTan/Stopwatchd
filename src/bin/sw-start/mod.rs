fbd647fdefca6db426bf666d9bb9690bec5bf76c
RenoirTan <renoirtan2005@gmail.com>
2023-01-31 18:16:38 +0800

    sockets: failed attempt at 2-way communication

--- src/bin/sw-start/mod.rs
+++ src/bin/sw-start/mod.rs
@@ -1,8 +1,16 @@
-use std::process;
+use std::{
+    process,
+    os::unix::net::{UnixStream, UnixListener},
+    io::{Write, Read}
+};
 
 #[macro_use]
 extern crate log;
-use stopwatchd::{logging::{create_syslogger, setup_syslogger, set_panic_hook}, pidfile::{open_pidfile, get_swd_pid}};
+use stopwatchd::{
+    logging::{create_syslogger, setup_syslogger, set_panic_hook},
+    pidfile::{open_pidfile, get_swd_pid},
+    runtime::server_socker_path
+};
 
 fn main() {
     let pid = process::id();
@@ -19,4 +27,23 @@ fn main() {
         get_swd_pid(&mut pidfile).unwrap()
     };
     info!("swd_pid is {}", swd_pid);
+
+    info!("creating client socket");
+    let csock_path = format!("/tmp/stopwatchd/sw-start_{}", pid);
+    let listener = UnixListener::bind(&csock_path).unwrap();
+
+    let ssock_path = server_socker_path(Some(swd_pid));
+    info!("creating stream");
+    let mut stream = UnixStream::connect(&ssock_path).unwrap();
+
+    info!("writing message to server");
+    stream.write_all(format!("{}//hi", csock_path).as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    info!("waiting for response from server");
+    let (mut response_stream, _response_saddr) = listener.accept().unwrap();
+    let mut response = String::new();
+    info!("reading response from server");
+    response_stream.read_to_string(&mut response).unwrap();
+    println!("{}", response);
 }
--- src/bin/swd/socket.rs
+++ src/bin/swd/socket.rs
@@ -1,9 +1,26 @@
 use std::{
-    path::Path,
+    path::{Path, PathBuf},
     os::unix::net::{UnixListener, UnixStream},
-    io::{self, Read},
+    io::{self, Read, Write},
     fs::remove_file
 };
+
+#[derive(Clone, Debug)]
+pub struct ClientInfo {
+    pub csock_path: PathBuf,
+    pub message: String
+}
+
+impl ClientInfo {
+    pub fn from_client_raw<S: AsRef<str>>(raw: S) -> io::Result<Self> {
+        let raw = raw.as_ref();
+        let dslash_index = raw.find("//")
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, raw))?;
+        let csock_path = PathBuf::from(&raw[..dslash_index]);
+        let message = &raw[(dslash_index+2)..];
+        Ok(ClientInfo {csock_path, message: message.to_string()})
+    }
+}
 
 pub fn clear_socket<P: AsRef<Path>>(path: &P) -> io::Result<()> {
     let path = path.as_ref();
@@ -19,18 +36,32 @@ pub fn create_socket<P: AsRef<Path>>(path: &P) -> io::Result<UnixListener> {
 }
 
 fn handle_client(client: &mut UnixStream) {
-    let mut message = String::new();
-    match client.read_to_string(&mut message) {
+    let mut raw = String::new();
+    match client.read_to_string(&mut raw) {
         Ok(bytes_read) => {
-            message = format!("client sent {} bytes: {}", bytes_read, message);
+            let message = format!("client sent {} bytes: {}", bytes_read, raw);
+            println!("{}", message);
+            info!("{}", message);
+            let client_info = match ClientInfo::from_client_raw(&raw) {
+                Ok(ci) => ci,
+                Err(e) => {error!("{}", e); return;}
+            };
+            debug!("client_info: {:?}", client_info);
+            let mut reply_socket = match UnixStream::connect(&client_info.csock_path) {
+                Ok(rs) => rs,
+                Err(e) => {panic!("{}", e);}
+            };
+            debug!("reply_socket for {:?} created", client_info.csock_path);
+            match reply_socket.write_all(b"thank you") {
+                Ok(_) => info!("message sent back to client"),
+                Err(e) => error!("{}", e)
+            };
         },
         Err(e) => {
-            message = format!("could not store client UnixStream in message: {}", e);
+            let message = format!("could not store client UnixStream in message: {}", e);
             error!("{}", message);
         }
     };
-    println!("{}", message);
-    info!("{}", message);
 }
 
 pub fn listen_to_socket(listener: &UnixListener) {
