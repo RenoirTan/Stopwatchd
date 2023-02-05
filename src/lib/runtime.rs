b289f92fdd1c2f19728d8e001875999588473ecf
RenoirTan <renoirtan2005@gmail.com>
2023-02-05 19:26:35 +0800

    main: change server sock file naming

--- src/lib/runtime.rs
+++ src/lib/runtime.rs
@@ -6,7 +6,7 @@ pub const DEFAULT_PIDFILE_PATH: &'static str = "/tmp/stopwatchd/pidfile";
 pub fn server_socket_path(pid: Option<u32>) -> PathBuf {
     let path = PathBuf::from(DEFAULT_RUNTIME_PATH);
     match pid {
-        Some(pid) => path.join(format!("server_socket_{}", pid)),
-        None => path.join(format!("server_socket"))
+        Some(pid) => path.join(format!("swd.{}.sock", pid)),
+        None => path.join("swd.sock")
     }
 }
