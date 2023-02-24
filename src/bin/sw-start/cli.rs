298448a6e448bc2d6b9f7f681470b71f0d54104b
RenoirTan <renoirtan2005@gmail.com>
2023-02-24 22:07:12 +0800

    main: change cli description for sw-start's name argument

--- src/bin/sw-start/cli.rs
+++ src/bin/sw-start/cli.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    /// Name of the new stopwatch, at most 6 (ASCII) characters long
+    /// Name of the new stopwatch, must be valid UTF-8
     pub name: Option<String>,
 
     /// Set verbosity to get more detailed info
