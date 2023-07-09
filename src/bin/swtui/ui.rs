76d2bf18b4eae7c108c187a82fc5976b928024c6
RenoirTan <renoirtan2005@gmail.com>
2023-07-09 17:29:45 +0800

    tui: dimensions

--- src/bin/swtui/ui.rs
+++ src/bin/swtui/ui.rs
@@ -11,6 +11,28 @@ impl Ui {
         self.window.printw("hello");
         self.window.refresh();
     }
+
+    /// (rows, columns) or (y, x)
+    pub fn dimensions(&self) -> (i32, i32) {
+        self.window.get_max_yx()
+    }
+
+    /// (top-left y, top-left x, bottom-right y, bottom-right x)
+    pub fn catalog_panel_geometry(&self) -> (i32, i32, i32, i32) {
+        let (wy, wx) = self.dimensions();
+        (0, 0, wy, wx/3)
+    }
+
+    /// (top-left y, top-left x, bottom-right y, bottom-right x)
+    pub fn focus_panel_geometry(&self) -> (i32, i32, i32, i32) {
+        let (wy, wx) = self.dimensions();
+        (0, (wx/3)+2, wy, wx)
+    }
+
+    pub fn separator_x(&self) -> i32 {
+        let (_wy, wx) = self.dimensions();
+        (wx/3)+1
+    }
 }
 
 impl AsRef<pancurses::Window> for Ui {
