9fe3a2909f609f548ae6a7ba092b815c077e3e91
RenoirTan <renoirtan2005@gmail.com>
2023-09-30 20:11:59 +0800

    tui: more cases

--- src/bin/swtui/ui/focus_panel.rs
+++ src/bin/swtui/ui/focus_panel.rs
@@ -16,17 +16,30 @@ impl FocusPanel {
     }
 
     pub fn draw(&self, ui: &Ui, state: &FocusPanelState) {
-        let FocusPanelState { selected: _selected, details } = state;
+        let FocusPanelState { selected, details } = state;
         self.clear(ui);
         let (left, right, top, bottom) = ui.borders_geometry().focus_panel_geometry();
         if let Some(ref d) = details {
             let display_name = d.identifier.to_string();
             let (l_x, r_x) = center_text(display_name.len(), (left, right)).unwrap();
-            ui.window.mvaddnstr(top, l_x as i32, &display_name, (r_x - l_x + 1) as i32);
+            ui.window.mvaddnstr(top, l_x, &display_name, r_x - l_x + 1);
             let row_filler = "x".repeat((right - left + 1) as usize);
             for y in top+1..=bottom {
                 ui.window.mvaddnstr(y, left, &row_filler, row_filler.len() as i32);
             }
+        } else if let Some(identifier) = selected.as_ref() {
+            let err_msg = "Could not find:";
+            let mid_y = (bottom - top) / 2;
+            let (l_x, r_x) = center_text(err_msg.len(), (left, right)).unwrap();
+            ui.window.mvaddnstr(mid_y, l_x, err_msg, r_x - l_x + 1);
+            let id_str = identifier.to_string();
+            let (l_x, r_x) = center_text(id_str.len(), (left, right)).unwrap();
+            ui.window.mvaddnstr(mid_y + 1, l_x, id_str, r_x - l_x + 1);
+        } else {
+            let welcome = "Stopwatchd";
+            let mid_y = (bottom - top) / 2;
+            let (l_x, r_x) = center_text(welcome.len(), (left, right)).unwrap();
+            ui.window.mvaddnstr(mid_y, l_x, welcome, r_x - l_x + 1);
         }
     }
 }
