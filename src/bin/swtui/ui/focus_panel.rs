c18d3e6db485d65ea0138ec9db41cd8b7fab7ae2
RenoirTan <renoirtan2005@gmail.com>
2023-11-17 16:21:00 +0800

    tui: kinda of a committed stash

--- src/bin/swtui/ui/focus_panel.rs
+++ src/bin/swtui/ui/focus_panel.rs
@@ -21,13 +21,15 @@ impl FocusPanel {
         let (left, right, top, bottom) = ui.borders_geometry().focus_panel_geometry();
         ColorPair::Active.set_color(&ui.window, false);
         if let Some(ref d) = details {
+            // Name
             let display_name = d.identifier.to_string();
             let (l_x, r_x) = center_text(display_name.len(), (left, right)).unwrap();
+            ColorPair::Active.set_color(&ui.window, true);
             ui.window.mvaddnstr(top, l_x, &display_name, r_x - l_x + 1);
-            let row_filler = "x".repeat((right - left + 1) as usize);
-            for y in top+1..=bottom {
-                ui.window.mvaddnstr(y, left, &row_filler, row_filler.len() as i32);
-            }
+
+            // Time
+            ColorPair::Active.set_color(&ui.window, false);
+            let _display_time = d.total_time;
         } else if let Some(identifier) = selected.as_ref() {
             let err_msg = "Could not find:";
             let mid_y = (bottom - top) / 2;
